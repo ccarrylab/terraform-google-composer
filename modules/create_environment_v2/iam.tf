695eaeb4e50a56345c837acda2fb600ea60dc092
Cohen Carryl <cohen.carryl@gmail.com>
2022-12-07 11:17:06 -0500

    Update iam.tf

--- modules/create_environment_v2/iam.tf
+++ modules/create_environment_v2/iam.tf
@@ -15,7 +15,7 @@
  */
 
 data "google_project" "project" {
-  project_id = "natural-metric-360918"
+  project_id = "citric-sprite-370915"
 }
 
 resource "google_project_iam_member" "composer_agent_service_account" {
